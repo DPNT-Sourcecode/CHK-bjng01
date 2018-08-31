0de77c0a0cea73c4ffce60a1376fd195b6526b33
sbx_user1059 <sbx_user1059@ip-10-128-41-39.eu-west-2.compute.internal>
2018-08-31 10:07:49 +0000

    Fri Aug 31 10:07:49 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,14 +5,42 @@ package befaster.solutions.CHK;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         int sum = 0;
+        int numA = 0;
+        int numB = 0;
+        int numC = 0;
+        int numD = 0;
     	
         //parse SKU string
+        char[] items = skus.toCharArray();
         
         //iterate over elements
+        for (int i=0; i<items.length; i++) {
+        	switch (items[i]) {
+        	case 1: items[i] = "A".charAt(0);
+        		numA +=1;
+        		break;
+        		
+        	case 2: items[i] = "B".charAt(0);
+    			numB +=1;
+    			break;
+    		
+        	case 3: items[i] = "C".charAt(0);
+    			numC +=1;
+    			break;
+    		
+        	case 4: items[i] = "D".charAt(0);
+    			numD +=1;
+    			break;
+        		
+        	default:
+        		return -1; //invalid input in given string
+        		
+        	}
+        }
+        //calculate discounts
         
         
     	return sum;
-    	
-    	//return -1;
+
     }
 }

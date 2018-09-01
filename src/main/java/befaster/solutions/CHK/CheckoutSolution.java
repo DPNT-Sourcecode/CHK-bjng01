590f9a2383e1991af8ac9c7b7d251cfa55a081bb
sbx_user1059 <sbx_user1059@ip-10-129-81-38.eu-west-2.compute.internal>
2018-09-01 13:26:50 +0000

    Sat Sep 01 13:26:50 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -55,10 +55,13 @@ public class CheckoutSolution {
         	numA -= 3;
         	sum += 130;
         }
-        
+        //infinite loop...
+        //need a count of E's
+        int cpyNumE = numE;
         //similarly, E before B
-        while (numE >=2) {
+        while (cpyNumE >=2) {
         	numB -= 1;
+        	cpyNumE -=2;
         	//no sum increase; decrease chargeable B's by 1 for every 2 E's
         }
         
